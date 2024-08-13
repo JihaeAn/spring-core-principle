0e023aa064469a0d7f03ca87da0572cb46124443
jihae <dkswlgo6615@naver.com>
2024-08-13 17:38:30 +0900

    0813 옵션 처리, 생성자 주입, 롬복과 최신 트렌드

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -10,14 +10,15 @@ import org.springframework.stereotype.Component;
 
 @Configuration
 @ComponentScan(
-        basePackages = "hello.core.member",
+        // 여기서 basePackages = "hello.core.member" 해줬다가 AutoAppConfigTest에서 생성자에 soutv 찍어봤을 때 안 됐었다 ! 유의 !
+        basePackages = "hello.core",
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         // 참고: 보통 설정 정보를 컴포넌트 스캔 대상에서 제외하지 않지만, 기존 예제 코드를 최대한 남기고 유지하기 위해 !!
 )
 public class AutoAppConfig {
 
-    @Bean(name = "memoryMemberRepository")
-    MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
-    }
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }
---
+++ src/main/java/hello/core/HelloLombok.java
@@ -0,0 +1,21 @@
+package hello.core;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+public class HelloLombok {
+
+    private String name;
+    private int age;
+
+    public static void main(String[] args) {
+        HelloLombok helloLombok = new HelloLombok();
+        helloLombok.setName("지해짱");
+
+        System.out.println("helloLombok = " + helloLombok);
+    }
+}
